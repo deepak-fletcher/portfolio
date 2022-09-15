44ea8fe2149ba0b0cd3690c29e183932ed76a64a
deepak-fletcher <deepakraofletcher@live.com>
2022-09-15 14:07:24 +0530

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,40 @@
 import React from 'react'
-import react from '../assets/react.png';
-import tailwind from '../assets/Tailwind.png';
+import github from '../assets/github.png';
+import linkedin from '../assets/linkedin.png';
 
 const Footer = () => {
   return (
     <div class="w-[100%] h-[100px] border-y-2 bg-[#000000] flex flex-row">
-        <p class="text-white m-auto w-[25%]">Portfolio of Deepak Rao Fletcher</p>
+        <p class="text-white m-auto w-[25%] font-bold text-xs">Portfolio of Deepak Rao Fletcher</p>
         <div class="w-[50%] m-auto">
-            <p class="text-white">
+            <p class="text-white font-bold text-xs">
                 Designed using
             </p>
-            <span class="m-auto mt-2 w-[100px] flex flex-row">
-            <img class="m-auto w-10 l-10 align-middle" id="react" className="image" src={react}></img>
-            <img class="m-auto w-7 l-7 align-middle" id="tailwind" className="image" src={tailwind}></img>
+            <span class="m-auto mt-2 w-[150px] flex flex-row">
+            <p class="text-white m-auto font-bold  text-[10px]">
+                ReactJS
+            </p>
+            <p class="text-white font-bold m-auto text-xs">
+                |
+            </p>
+            <p class="text-white font-bold m-auto text-[10px]">
+                TailwindCSS
+            </p>
             </span>
             
         </div>
         <div class="w-[25%] m-auto">
-            <p class="text-white">
+            <p class="text-white m-auto w-[100px] mb-2 text-xs font-bold">
                 Find me on
             </p>
+            <div class="flex w-[100px] m-auto flex-row">
+            <a class="m-auto" href="https://github.com/deepak-fletcher">
+                <img class="w-[15px] h-[15px] m-auto" id="github" className="image" src={github} ></img>
+            </a>
+            <a class="m-auto" href="https://www.linkedin.com/in/deepak-fletcher/">
+                <img class="w-[15px] h-[15px] m-auto" id="linkedin" className="image" src={linkedin} ></img>
+            </a>
+            </div>
             
         </div>
     </div>
